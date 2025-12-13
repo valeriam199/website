c1f1e1771cd4f3a692c9f60a5fa5e707e42a8a3e
Valeria <valeria.muggironi@student.htw-berlin.de>
2025-12-13 16:25:34 +0100

    fixed folder path

--- webserver.js
+++ webserver.js
@@ -2,6 +2,11 @@ const http = require('http');
 const fs = require('fs');
 const timestamp = require('time-stamp');
 
+const exportfolder = './exports';
+if (!fs.existsSync(exportfolder)) {
+    fs.mkdirSync(exportfolder);
+}
+
 const server = http.createServer(function (request, response){
     if(request.method === "POST" && request.url==="/export"){
         console.log("Korrekte Anfrage!---------------------------")
@@ -11,7 +16,7 @@ const server = http.createServer(function (request, response){
         });
 
         request.on('end', function(){
-            const filename = "export_" + timestamp('YYYYMMDDHHmmss') + ".json";
+            const filename = exportfolder + "/export_" + timestamp('YYYYMMDDHHmmss') + ".json";
             fs.writeFile(filename, daten, function(err){
                 if(err){
                     return console.error(err);
@@ -28,19 +33,6 @@ const server = http.createServer(function (request, response){
 }).listen(8081);
 console.log("Der Webserver ist erreichbar unter http://127.0.0.1:8081/");
 
-setTimeout(function(){
-    const testDaten = '{"name":"Test"}';
-
-    const req = http.request({
-        hostname: '127.0.0.1',
-        port: 8081,
-        path: '/export',
-        method: 'POST',
-
-    });
-    req.write(testDaten);
-    req.end()
-});
 
 
 
